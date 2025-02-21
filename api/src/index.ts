3dd8847753b50f62d578c81a45a621545f74d831
pranav514 <pranavsalunkhe327@gmail.com>
2025-02-21 12:00:00 +0530

    added kline route and corn job to refresh the materialized view

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import  userRoute from "./routes/userRoute";
 import orderRoute from "./routes/orderRoute";
 import depthRoute from "./routes/depthRoute";
+import klineRoute from "./routes/klineRoute";
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(cors());
 app.use('/api/v1/user' , userRoute)
 app.use('/api/v1/order' , orderRoute)
 app.use('/api/v1/depth' , depthRoute)
+app.use('/api/v1/klines', klineRoute)
 
 app.listen(3000 , () => {
     console.log("api started at the port 3000");
